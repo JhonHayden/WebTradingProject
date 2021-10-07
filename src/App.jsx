0c8ce0bbab87dd289fc370dde89374a7948e15be
Jhon Hayden Ospina Loaiza <jhonhayden@hotmail.com>
2021-10-07 02:24:54 -0500

    prueba uso de contexto y useRef y FormData

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import 'styles/styles.css';
-import {BrowserRouter as Router,Switch,Route } from 'react-router-dom';  // importo libreria para navegacion 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';  // importo libreria para navegacion 
 import PrivateLayout from 'layouts/PrivateLayout';
 import AuthLayout from 'layouts/AuthLayout';
 import PublicLayout from 'layouts/PublicLayout';
@@ -12,58 +12,61 @@ import MaestroUsuario from 'pages/administracion/MaestroUsuario';
 import Admin from 'pages/administracion/Index';
 import Login from 'pages/Login';
 import Index from 'pages/Index';
+import { DarkModeContext } from 'contexto/DarkMode'
 
-function App() { 
-  return (  
+function App() {
+  return (
+    <div>
+      <DarkModeContext.Provider>
+        <Router>
+          <Switch>
+            <Route path={['/administracion/maestroproductos', '/administracion/maestroproductosactualizar',
+              '/administracion/maestrousuario', '/administracion/ventas', '/administracion/registroproductos',
+              '/administracion/moduloVentas', '/administracion']}>
+              <PrivateLayout>
+                <Switch>
+                  <Route path='/administracion/maestroproductos'>
+                    <MaestroProductos />
+                  </Route>
+                  <Route path='/administracion/maestroproductosactualizar'>
+                    <MaestroProductosActualizar />
+                  </Route>
+                  <Route path='/administracion/maestrousuario'>
+                    <MaestroUsuario />
+                  </Route>
+                  <Route path='/administracion/maestroventas'>
+                    <MaestroVentas />
+                  </Route>
+                  <Route path='/administracion/registroproductos'>
+                    <RegistroProductos />
+                  </Route>
+                  <Route path='/administracion/moduloVentas'>
+                    <ModuloVentas />
+                  </Route>
+                  <Route path='/administracion'>
+                    <Admin />
+                  </Route>
+                </Switch>
+              </PrivateLayout>
+            </Route>
+            <Route path='/login'>
+              <AuthLayout>
+                <Login />
+              </AuthLayout>
+            </Route>
+            <Route path='/'>
+              <PublicLayout>
+                <Index />
+              </PublicLayout>
+            </Route>
+          </Switch>
+        </Router>
+      </DarkModeContext.Provider>
+    </div>
 
-    <Router>
-      <Switch>
-        <Route path={['/administracion/maestroproductos','/administracion/maestroproductosactualizar',
-        '/administracion/maestrousuario','/administracion/ventas','/administracion/registroproductos',
-        '/administracion/moduloVentas','/administracion']}>
-          <PrivateLayout>
-            <Switch>
-              <Route path='/administracion/maestroproductos'>
-                <MaestroProductos/>
-              </Route>
-              <Route path='/administracion/maestroproductosactualizar'>
-                <MaestroProductosActualizar/>
-              </Route>
-              <Route path='/administracion/maestrousuario'>
-                <MaestroUsuario/>
-              </Route>
-              <Route path='/administracion/maestroventas'>
-                <MaestroVentas/>
-              </Route>
-              <Route path='/administracion/registroproductos'>
-                <RegistroProductos/>
-              </Route>
-              <Route path='/administracion/moduloVentas'>
-                <ModuloVentas/>
-              </Route>
-              <Route path='/administracion'>
-                <Admin/>
-              </Route>
-            </Switch>
-          </PrivateLayout>
-        </Route>
-        <Route path='/login'>
-          <AuthLayout>
-            <Login/>
-          </AuthLayout>
-        </Route>
-        <Route path= '/'>
-          <PublicLayout>
-            <Index/>
-          </PublicLayout>
-        </Route>
-      </Switch>
-    </Router>
-      
   );
 }
 
 export default App;
 
 
- 
